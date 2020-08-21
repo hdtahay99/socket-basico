887733b30d46b01b47dc9f51d6230bacef5189ac
Heansell Tahay <hdtahay@gmail.com>
2020-08-20 19:17:12 -0600

    primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,23 @@
+let socket = io();
+
+// Escuchar sucesos
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Heansell',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+//Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor', mensaje);
+});
---
+++ server/sockets/socket.js
@@ -0,0 +1,35 @@
+const { io } = require('../server');
+
+io.on('connection', (client) => {
+
+    console.log('Usuario conectado');
+
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido a esta aplicación'
+    });
+
+    client.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+
+    // Escuchar al cliente
+    client.on('enviarMensaje', (data, callback) => {
+        console.log(data);
+
+        client.broadcast.emit('enviarMensaje', data);
+
+        // if (data.usuario) {
+        //     callback({
+        //         respuesta: 'TODO SALIÓ BIEN'
+        //     });
+        // } else {
+        //     callback({
+        //         respuesta: 'TODO SALIÓ MAL!'
+        //     });
+        // }
+
+
+    });
+
+});
